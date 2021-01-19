f1819c78b6dc6a129f8d50822cdbe3f9c6256279
unknown <katurijagadeesshabu27@gmail.com>
2021-01-19 18:52:21 +0530

    add

---
+++ src/org/test/Sample.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class Sample {
+	
+	
+	
+	private void add() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+	private void hf() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
